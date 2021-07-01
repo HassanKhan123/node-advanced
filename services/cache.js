25933d925317b9366fbcef7728268a726e90e06a
HassanKhan123 <hk2602809@gmail.com>
2021-07-01 21:20:35 +0500

    cache implementation

--- services/cache.js
+++ services/cache.js
@@ -1,15 +1,26 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-  console.log('MONGOOSE QUERY RAN');
+mongoose.Query.prototype.exec = async function () {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    }),
+  );
 
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name,
-  });
+  const cacheValue = await client.get(key);
+  if (cacheValue) {
+    console.log('CACHE========', cacheValue);
+  }
 
-  console.log('key==', key);
-
-  return exec.apply(this, arguments);
+  const result = await exec.apply(this, arguments);
+  console.log('RES=======', result);
+  client.set(key, result);
 };
