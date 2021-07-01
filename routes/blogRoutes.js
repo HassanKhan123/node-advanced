fc3f482358f806c02f6a04501f7214518563c516
HassanKhan123 <hk2602809@gmail.com>
2021-07-01 14:02:21 +0500

    implemented redis caching

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -23,10 +23,16 @@ module.exports = app => {
     client.get = util.promisify(client.get);
 
     const cachedBlogs = await client.get(req.user.id);
+    if (cachedBlogs) {
+      console.log('SERVING FROM CACHE');
+      return res.send(JSON.parse(cachedBlogs));
+    }
 
     const blogs = await Blog.find({ _user: req.user.id });
-
+    console.log('SERVING FROM DB');
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
